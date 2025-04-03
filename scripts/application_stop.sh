46c5a0aad7aac01e89e3bc4649d7f6e983d907bf
Rishi Chauhan <rishichauhan1419@gmail.com>
2025-04-03 15:16:22 +0530

    Remove Unnecceray

--- scripts/application_stop.sh
+++
@@ -1,7 +0,0 @@
-# # scripts/application_stop.sh
-# #!/bin/bash
-# echo "Stopping application..."
-# pm2 stop all || echo "No process running"
-#!/bin/bash
-echo "Stopping application..." | tee -a /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-pm2 stop all || echo "No process running" | tee -a /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
